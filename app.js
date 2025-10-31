4b42148eae9e2e2abbc5075c2a31b9525e531695
visalkorn22 <kornvisal222@gmail.com>
2025-10-31 11:12:44 +0700

    Update greeting message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, GitHub Actions!");
+  res.send("Hello, CI/CD!");
 });
 
 module.exports = app;
