684abfc0a5458c93dbf305b56e55725fe6468aa3
visalkorn22 <kornvisal222@gmail.com>
2025-10-31 12:05:11 +0700

    Deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, CI/CD!");
+  res.send("Hello, VISAL!");
 });
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const request = require("supertest");
 const app = require("../app");
 const { expect } = require("chai");
 describe("GET /", () => {
-  it("should return Hello, CI/CD!", async () => {
+  it("should return Hello, VISAL!", async () => {
     const res = await request(app).get("/");
     expect(res.status).to.equal(200);
-    expect(res.text).to.equal("Hello, CI/CD!"); // <-- updated
+    expect(res.text).to.equal("Hello, VISAL!"); // <-- updated
   });
 });
